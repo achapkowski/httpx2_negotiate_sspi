4c921e70b5a96ac9a7a944d9197c20ee892da037
andrew <achapkowski@esri.com>
2026-07-10 15:03:58 -0400

    code formatting fix release as stable.

--- docs/conf.py
+++ docs/conf.py
@@ -5,11 +5,11 @@ import sys
 
 sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
 
-project = 'httpx2-negotiate-sspi'
-author = 'achapkowski'
+project = "httpx2-negotiate-sspi"
+author = "achapkowski"
 
-extensions = ['sphinx.ext.autodoc']
-autodoc_mock_imports = ['pywintypes', 'sspi', 'sspicon', 'win32security']
-autoclass_content = 'init'
+extensions = ["sphinx.ext.autodoc"]
+autodoc_mock_imports = ["pywintypes", "sspi", "sspicon", "win32security"]
+autoclass_content = "init"
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
